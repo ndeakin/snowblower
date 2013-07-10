c0930946f5200aa63e1d382a2bc2ee832dd70aad
Nick <nickrdeakin@gmail.com>
2013-07-09 21:33:27 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Snowblower::Application.config.secret_token = 'b0f8242f2062b0961fa63540faceb01625877f1bde5167487a879c571ea10a276d89b6603cdaf3227bec1b50feefc8cd534be3230bad2d50c4b9c6dafc2341aa'
